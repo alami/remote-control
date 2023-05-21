758507179523b113099f9c39852fc96dea752e76
alami <alami@km.ru>
2023-05-21 16:11:49 +0500

    init wss & fst events

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import { httpServer } from "./http_server/index";
 import "dotenv/config"
+import {createWebSocketStream, WebSocket, WebSocketServer} from "ws";
 
 const HTTP_PORT = Number(process.env.HTTP_PORT) || 8181;
 const WS_PORT = Number(process.env.WS_PORT) || 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({port: WS_PORT});
+
+try {
+    wss.on("connection", async (ws: WebSocket)=>{
+        const stream = createWebSocketStream(ws, {encoding: 'utf8', decodeStrings: false})
+        console.log(`WS-server was started on ${WS_PORT} port`)
+        ws.send(`ws_start ${WS_PORT}`)
+        ws.on('close', ()=>{
+            console.log('WS closed')
+        })
+    })
+} catch (e) {
+    console.log('Error: ', e)
+}
+process.on('SIGINT',()=>{
+    console.log('Sorry, WS-server will be closed.')
+    wss.close()
+    process.exit()
+})
+
