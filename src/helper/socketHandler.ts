7f1f07bfd0c5cf4c078a5e661bee9bc655ab82a7
alami <alami@km.ru>
2023-05-21 18:38:49 +0500

    add screenEvent

--- src/helper/socketHandler.ts
+++ src/helper/socketHandler.ts
@@ -1,6 +1,7 @@
 import {Duplex} from "stream";
 import mouseEvent from "./mouseEvent";
 import drawEvent from "./drawEvent";
+import screenEvent from "./screenEvent";
 export default async function socketHandler (ch: string, stream: Duplex) {
     const [cmd, pos, pos2] = ch.split(' ')
     if (cmd.startsWith('mouse')) {
@@ -8,5 +9,8 @@ export default async function socketHandler (ch: string, stream: Duplex) {
     }
     else if (cmd.startsWith('draw')) {
         await drawEvent(cmd, pos, pos2, stream)
+    }
+    else {
+        await screenEvent(cmd, stream);
     }
 }
